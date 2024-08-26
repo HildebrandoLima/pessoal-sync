67dd8a8c1c21fde97b7f03103d6bfba436e64add
HildebrandoLima <hildebrandolima16@gmail.com>
2024-08-26 11:38:07 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/com/br/pessoal_sync/PessoalSyncApplication.java
@@ -0,0 +1,13 @@
+package com.br.pessoal_sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoalSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoalSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/pessoal_sync/PessoalSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.pessoal_sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoalSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
